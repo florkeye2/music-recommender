e6fa5ee4adfee5b0d6068cf2cc95f692c806621c
memeding1@gmail.com <florkeye2@udayton.edu>
2025-03-13 23:13:45 -0400

    Added some blank stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,9 @@ import "./tailwind.css"
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainPage/>}/>
+      <Route path="/register" element={<h1>Register Here!</h1>}/>
       <Route path="/login" element={<h1>Login Here!</h1>}/>
+      <Route path="/home" element={<MainPage/>}/>
     </Routes>
   );
 }
